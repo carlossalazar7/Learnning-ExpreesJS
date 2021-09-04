e13fbd86138d9599937dc0a0a5c30a9ac3509a0f
carlossalazar7 <carlossalazar2228@gmail.com>
2021-09-04 15:12:18 -0600

    npm install dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-
+const mongoose = require('mongoose')
+const env = require('dotenv/config')
 // use este cuando envie por raw body
 app.use(express.json())
 //  use este cuando envie por x-www-form-urlencoded
@@ -13,4 +14,10 @@ app.use('/api/', useRouter)
 app.listen('3000', () => {
     console.log("The server is  running!!!");
 })
-
+//crear conexion a mongo db
+mongoose.connect(
+    process.env.DB, /* esta linea manda a llamar los paramtros de conecion en el archivo .env */
+    { useNewUrlParser: true, useUnifiedTopology: true }, (e) => {
+        if (e)  return console.log(e.message);
+        console.log('database connected');
+    })
